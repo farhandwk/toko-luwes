354818803537a2ed5b7a0655b40b15297bdb2190
farhandwk <farhandwiki15@gmail.com>
2026-01-13 10:09:35 +0700

    add suspense login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,21 @@
+import { Suspense } from 'react'; // <--- WAJIB IMPORT INI
 import LoginForm from "@/components/LoginForm";
+import { Loader2 } from "lucide-react"; // Ikon loading biar cantik
 
 export default function LoginPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-slate-100 p-4">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        
+        {/* BUNGKUS DENGAN SUSPENSE */}
+        <Suspense fallback={
+            <div className="flex justify-center p-8">
+                <Loader2 className="h-8 w-8 animate-spin text-slate-400" />
+            </div>
+        }>
+            <LoginForm />
+        </Suspense>
+
         <p className="mt-4 text-center text-xs text-slate-500">
           &copy; {new Date().getFullYear()} POS Toko Luwes
         </p>
